ea556cde2e34b2e586b21425d2bc174eff9654b0
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-28 14:57:46 -0700

    Enable & update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,39 @@
 'use strict';
-// 2019-06-11 12:25
+
 const config = {
-	version: location.hostname === 'localhost' ? new Date().toISOString() : '1.0.0',
+	version: '1.0.0',
 	stale: [
 		'/',
+		'/users.js',
 		'/js/index.js',
-		'/js/share-button.js',
-		'/js/share-config.js',
-		'/js/gravatar-img.js',
-		'/js/current-year.js',
-		'/js/imgur-img.js',
-		'/js/share-config.js',
-		'/css/styles/index.css',
-		'/css/styles/vars.css',
-		'/css/styles/layout.css',
-		'/css/styles/header.css',
-		'/css/styles/nav.css',
-		'/css/styles/main.css',
-		'/css/styles/sidebar.css',
-		'/css/styles/footer.css',
-		'/img/favicon.svg',
-		'/img/icons.svg',
-		'/img/apple-touch-icon.png',
-		'/img/logos/creative-common-by-sa.svg',
-		'/img/octicons/mail.svg',
+		'/components/pages/login.js',
+		'/components/pages/login.html',
+		'/components/error-message.js',
+		'/components/error-message.html',
+		'/components/toast-message.js',
+		'/components/toast-message.html',
+		'/components/claim-item.js',
+		'/components/claim-item.html',
+		'/components/pages/claims.js',
+		'/components/pages/claims.html',
+		'/components/pages/claims.json',
+		'/components/pages/claim.js',
+		'/components/pages/claim.html',
+		'/components/pages/forgot-password.js',
+		'/components/pages/forgot-password.html',
+		'/components/pages/register.js',
+		'/components/pages/register.html',
+		'/components/pages/account.js',
+		'/components/pages/account.html',
+		'/components/pages/profile.js',
+		'/components/pages/profile.html',
+		'/components/app-footer.js',
+		'/components/app-footer.html',
+		'/components/logout-button.js',
+		'/components/logo-img.js',
+		'/components/logo-img.html',
+		'/components/back-button.js',
+		'/js/routes.js',
 		'/js/std-js/deprefixer.js',
 		'/js/std-js/shims.js',
 		'/js/std-js/md5.js',
@@ -31,6 +41,14 @@ const config = {
 		'/js/std-js/webShareApi.js',
 		'/js/std-js/esQuery.js',
 		'/js/std-js/functions.js',
+		'/css/styles/index.css',
+		'/css/styles/vars.css',
+		'/css/styles/layout.css',
+		'/css/styles/header.css',
+		'/css/styles/nav.css',
+		'/css/styles/main.css',
+		'/css/styles/sidebar.css',
+		'/css/styles/common.css',
 		'/css/core-css/rem.css',
 		'/css/core-css/viewport.css',
 		'/css/core-css/element.css',
@@ -38,18 +56,32 @@ const config = {
 		'/css/core-css/utility.css',
 		'/css/core-css/fonts.css',
 		'/css/core-css/animations.css',
-		'/css/normalize.css/normalize.css',
+		'/css/normalize/normalize.css',
 		'/css/animate.css/animate.css',
+		'/img/icons.svg',
+		'/img/apple-touch-icon.png',
+		'/img/icon-192.png',
+		'/img/favicon.svg',
+		'/fonts/roboto.woff2',
 	].map(path => new URL(path, location.origin).href),
+	fresh: [
+		'https://cdn.polyfill.io/v2/polyfill.min.js?unknown=polyfill&features=es6,MutationObserver,IntersectionObserver,IntersectionObserverEntry,Object.values,Object.entries,NodeList.prototype.@@iterator,Array.prototype.@@iterator&flags=gated',
+		'https://unpkg.com/@webcomponents/custom-elements@1.2.4/custom-elements.min.js',
+	]
 };
 
 self.addEventListener('install', async () => {
-	const cache = await caches.open(config.version);
-	const keys = await caches.keys();
-	const old = keys.filter(k => k !== config.version);
-	await Promise.all(old.map(key => caches.delete(key)));
+	try {
+		for (const key of await caches.keys()) {
+			await caches.delete(key);
+		}
 
-	await cache.addAll(config.stale);
+		const cache = await caches.open(config.version);
+		await cache.addAll(config.stale).catch(console.error);
+	} catch (err) {
+		console.error(err);
+	}
+
 	skipWaiting();
 });
 
@@ -59,15 +91,43 @@ self.addEventListener('activate', event => {
 	}());
 });
 
-self.addEventListener('fetch', async event => {
-	async function get(request) {
-		const cache = await caches.open(config.version);
-		const cached = await cache.match(request);
-
-		return cached instanceof Response ? cached : fetch(request);
-	}
-
-	if (event.request.method === 'GET' && config.stale.includes(event.request.url)) {
-		event.respondWith(get(event.request));
+self.addEventListener('fetch', event => {
+	switch(event.request.method) {
+	case 'GET':
+		if (Array.isArray(config.stale) && config.stale.includes(event.request.url)) {
+			event.respondWith((async () => {
+				const cached = await caches.match(event.request);
+				if (cached instanceof Response) {
+					return cached;
+				} else {
+					return await fetch(event.request);
+				}
+			})());
+		} else if (Array.isArray(config.fresh) && config.fresh.includes(event.request.url)) {
+			event.respondWith((async () => {
+				if (navigator.onLine) {
+					const resp = await fetch(event.request);
+					const cache = await caches.open(config.version);
+					cache.add(resp.clone());
+					return resp;
+				} else {
+					return await caches.match(event.request);
+				}
+			})());
+		} else if (Array.isArray(config.allowed) && config.allowed.some(host => new URL(event.request.url).host === host)) {
+			event.respondWith((async () => {
+				const resp = await caches.match(event.request);
+				if (resp instanceof Response) {
+					return resp;
+				} else if (navigator.onLine) {
+					const resp = await fetch(event.request);
+					const cache = await caches.open(config.version);
+					cache.add(resp.clone());
+					return resp;
+				} else {
+					return await fetch(event.request);
+				}
+			})());
+		}
 	}
 });
